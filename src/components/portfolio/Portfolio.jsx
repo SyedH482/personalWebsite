338dda48b613c60a09fa0baac8350265b7d1988c
SyedH482 <syed.i.hussaini.24@dartmouth.edu>
2022-05-13 13:04:46 -0400

    more edits

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,7 +1,23 @@
 import './portfolio.scss';
 
-export default function portfolio() {
+export default function Portfolio() {
   return (
-    <div className="portfolio" id="portfolio">portfolio</div>
+    <div className="portfolio" id="portfolio">
+      <h1>Portfolio</h1>
+      <ul>
+        <li className='active'>Featured</li>
+        <li>Web App</li>
+        <li>Mobile App</li>
+        <li>Branding</li>
+        <li>Design</li>
+      </ul>
+
+      <div className="container">
+        <div className="item">
+          <img src="" alt=""/>
+          <h3>Banking App</h3>
+        </div>
+      </div>
+    </div>
   )
 }
