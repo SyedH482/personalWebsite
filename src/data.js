30f1464c323bf4cf88278e8af32d49216fee56cc
SyedH482 <syed.i.hussaini.24@dartmouth.edu>
2022-05-23 03:03:20 -0400

    content done need edits

--- src/data.js
+++ src/data.js
@@ -31,9 +31,9 @@ export const featuredPortfolio = [
   export const mobilePortfolio = [
     {
       id: 1,
-      title: "Mobile Social Media App",
+      title: "Android Flashcard App",
       img:
-        "https://cdn.dribbble.com/users/2597268/screenshots/15468893/media/44313553d73ba41580f1df69749bba28.jpg?compress=1&resize=1200x900",
+        "../../assets/flashcardApp.png",
     },
   
   ];
